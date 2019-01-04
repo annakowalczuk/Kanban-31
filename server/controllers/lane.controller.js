3998685ae7200e4d3f641836a47fdeb9fa01ce20
Anna Kowalczuk <anna@kowalczuk.eu>
2019-01-04 11:46:31 +0100

    Initial commit

---
+++ server/controllers/lane.controller.js
@@ -0,0 +1,64 @@
+import Lane from '../models/lane';
+import Note from '../models/note';
+import uuid from 'uuid';
+
+export function addLane(req, res) {
+  if (!req.body.name) {
+    res.status(403).end();
+  }
+
+  const newLane = new Lane(req.body);
+
+  newLane.notes = [];
+
+  newLane.id = uuid();
+  newLane.save((err, saved) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.json(saved);
+  });
+}
+
+export function getLanes(req, res) {
+  Lane.find().exec((err, lanes) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.json({ lanes });
+  });
+}
+
+export function deleteLane(req, res) {
+  Lane.findOne({ id: req.params.laneId }).exec((err, lane) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+
+    lane.notes.forEach(noteToRemove => {
+      Note.findByIdAndRemove(noteToRemove._id).exec((error) => {
+        if (error) {
+          res.status(500).send(error);
+        }
+      });
+    }
+  );
+
+    lane.remove(() => {
+      res.status(200).end();
+    });
+  });
+}
+
+export function editLane(req, res) {
+  if (!req.body.name) {
+    res.status(403).end();
+  }
+
+  Lane.update({ id: req.params.laneId }, { $set: { name: req.body.name } }).exec((err, lane) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.json(lane);
+  });
+}
---
+++ server/controllers/note.controller.js
@@ -0,0 +1,56 @@
+import Note from '../models/note';
+import Lane from '../models/lane';
+import uuid from 'uuid';
+
+
+export function addNote(req, res) {
+  const { note, laneId } = req.body;
+
+  if (!note || !note.task || !laneId) {
+    res.status(400).end();
+  }
+
+  const newNote = new Note({
+    task: note.task,
+  });
+
+  newNote.id = uuid();
+  newNote.save((err, saved) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    Lane.findOne({ id: laneId })
+      .then(lane => {
+        lane.notes.push(saved);
+        return lane.save();
+      })
+      .then(() => {
+        res.json(saved);
+      });
+  });
+}
+
+export function deleteNote(req, res) {
+  Note.findOne({ id: req.params.noteId }).exec((err, note) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+
+    note.remove(() => {
+      res.status(200).end();
+    });
+  });
+}
+
+export function editNote(req, res) {
+  if (!req.body.task) {
+    res.status(403).end();
+  }
+
+  Note.update({ id: req.params.noteId }, { $set: { task: req.body.task } }).exec((err, note) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.json(note);
+  });
+}
